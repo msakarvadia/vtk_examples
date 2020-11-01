a9acb6f8f9a9f351b7dcc4a7e5e7765eed0dd8b7
mansi <mansi@sunny.attlocal.net>
2020-11-01 09:37:15 -0500

    description of code added from vtk example website

--- color_cells.py
+++ color_cells.py
@@ -2,7 +2,25 @@
 #https://vtk.org/Wiki/VTK/Examples/Python/Visualization/AssignColorsCellFromLUT
 #changes to original code by Mansi Sakarvadia
 
-from __future__ import print_function
+'''
+frotrates how to assign colors to cells in a vtkPolyData structure using
+ lookup tables.
+Two techniques are demonstrated:
+1) Using a lookup table of predefined colors.
+2) Using a lookup table generated from a color transfer function.
+
+The resultant display shows in the left-hand column, the cells in a plane
+colored by the two lookup tables and in the right-hand column, the same
+polydata that has been read in from a file demonstrating that the structures
+are identical.
+
+The top row of the display uses the color transfer function to create a
+ green to tan transition in a diverging color space.
+ Note that the central square is white indicating the midpoint.
+The bottom row of the display uses a lookup table of predefined colors.
+'''
+
+from  __future__ import print_function
 import vtk
 
 def MakeLUT(tableSize):
